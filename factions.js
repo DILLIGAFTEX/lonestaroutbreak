449fb7661d94f465e018af2f457410b4a742d220
DILLIGAFTEX <129958883+DILLIGAFTEX@users.noreply.github.com>
2025-07-13 06:57:09 -0500

    Update factions.js

--- factions.js
+++ factions.js
@@ -10,35 +10,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const factionsData = {
     Medic: {
-      image: 'https://placehold.co/150x150/e0e0e0/b30000?text=Medic',
+      image: 'https://cdn.discordapp.com/attachments/1244574412376899614/1393923834146455685/medical.png?ex=6874f03b&is=68739ebb&hm=110c3e0040734400cb2dc684ccf6f5cf41e9156d5a8f280ebe7edc784196965e&',
       perks: [
         "Can revive downed allies faster.",
         "Crafts advanced medical supplies."
       ]
     },
     Engineer: {
-      image: 'https://placehold.co/150x150/e0e0e0/b30000?text=Engineer',
+      image: 'https://cdn.discordapp.com/attachments/1244574412376899614/1393923804853571715/medical.png?ex=6874f034&is=68739eb4&hm=4308b2ede80633223c064c7b16c229827da22b94d323cb70954a1b0b5428d442&',
       perks: [
         "Can repair vehicles and structures.",
         "Crafts defensive fortifications."
       ]
     },
     Recon: {
-      image: 'https://placehold.co/150x150/e0e0e0/b30000?text=Recon',
+      image: 'https://cdn.discordapp.com/attachments/1244574412376899614/1393923843633971200/recon.png?ex=6874f03e&is=68739ebe&hm=c6841dda060c42ab37afdbb7a19b4e740ffcafc228b4cee0240d4e5c48af7dba&',
       perks: [
         "Can spot and mark enemies for the team.",
         "Moves faster and more quietly."
       ]
     },
     Scavenger: {
-      image: 'https://placehold.co/150x150/e0e0e0/b30000?text=Scavenger',
+      image: 'https://cdn.discordapp.com/attachments/1244574412376899614/1393923853675266058/scavenger.png?ex=6874f040&is=68739ec0&hm=1239aa3210cd4ef9b315d955c4886f6f411bf89ffd0c42a9602e143903e5db06&',
       perks: [
         "Finds higher quality loot.",
         "Increased inventory capacity."
       ]
     },
     Hunter: {
-      image: 'https://placehold.co/150x150/e0e0e0/b30000?text=Hunter',
+      image: 'https://cdn.discordapp.com/attachments/1244574412376899614/1393923815884722277/hunter.png?ex=6874f037&is=68739eb7&hm=871f4d91360c31d1b4427db4637520894ea2686cc5d44f57319dc2c6038acf6e&',
       perks: [
         "Deals bonus damage to infected.",
         "Can track recent enemy movements."
